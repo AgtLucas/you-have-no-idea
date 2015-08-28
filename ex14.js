f2293054dd7ea15094970c09d751b91f446e7d0c
Lucas <lucas@agtlucas.com>
2015-08-28 08:15:06 -0300

    Update ex14.js

--- ex14.js
+++ ex14.js
@@ -1,5 +1,23 @@
 function repeat(operation, num) {
-
+  return function() {
+    if (num <= 0) {
+      return;
+    }
+    operation();
+    return repeat(operation, --num);
+  }
 }
 
-module.exports = repeat;
+function trampoline(fn) {
+  while(fn && typeof fn === 'function') {
+    fn = fn();
+  }
+}
+
+
+
+module.exports = function(operation, num) {
+  trampoline(function() {
+    return repeat(operation, num);
+  });
+}
