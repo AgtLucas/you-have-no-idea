838886157c5a4ef9d6fdda4178f7feb666b9f399
Lucas <AgtLucas@users.noreply.github.com>
2016-01-12 04:14:52 -0200

    Update ex12.js

--- ex12.js
+++ ex12.js
@@ -1,16 +1,16 @@
 function Spy(target, method) {
-  var originalFunction = target[method];
+  var originalFunction = target[method]
 
   var result = {
     count: 0
   }
 
   target[method] = function() {
-    result.count++;
-    return originalFunction.apply(this, arguments);
+    result.count++
+    return originalFunction.apply(this, arguments)
   }
 
-  return result;
+  return result
 }
 
-module.exports = Spy;
+module.exports = Spy
