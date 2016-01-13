3088f4150f49e8bd1497eb0236cb67a89a5f4043
Lucas <AgtLucas@users.noreply.github.com>
2016-01-13 12:41:42 -0200

    Update ex15.js

--- ex15.js
+++ ex15.js
@@ -1,14 +1,14 @@
 function loadUsers(usersIds, load, done) {
-  var completed = 0;
-  var users = [];
+  var completed = 0
+  var users = []
   usersIds.forEach(function(id, index) {
     load(id, function(user) {
-      users[index] = user;
+      users[index] = user
       if (++completed === usersIds.length) {
-        return done(users);
+        return done(users)
       }
-    });
-  });
+    })
+  })
 }
 
-module.exports = loadUsers;
+module.exports = loadUsers
