30bfb674fbdb34e1184c47f06eef435b73e4405a
Lucas <AgtLucas@users.noreply.github.com>
2016-01-15 07:33:09 -0200

    Update ex5.js

--- ex5.js
+++ ex5.js
@@ -2,10 +2,10 @@ function checkUsersValid(goodUsers) {
   return function allUsersValid(submittedUsers) {
     return submittedUsers.every(function(submittedUsers) {
       return goodUsers.some(function(goodUser) {
-        return goodUser.id === submittedUsers.id;
+        return goodUser.id === submittedUsers.id
       })
-    });
-  };
+    })
+  }
 }
 
-module.exports = checkUsersValid;
+module.exports = checkUsersValid
