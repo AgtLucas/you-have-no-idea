66179cdaa8d5c765aa67a1fe6cb43acafba45438
Lucas <AgtLucas@users.noreply.github.com>
2016-01-17 09:30:12 -0200

    Update ex7.js

--- ex7.js
+++ ex7.js
@@ -1,10 +1,10 @@
 function reduce(arr, fn, initial) {
   return (function reduceOne(index, value) {
     if (index > arr.length - 1) {
-      return value;
+      return value
     }
-    return reduceOne(index + 1, fn(value, arr[index], index, arr));
-  })(0, initial);
+    return reduceOne(index + 1, fn(value, arr[index], index, arr))
+  })(0, initial)
 }
 
-module.exports = reduce;
+module.exports = reduce
