5af2615a2736a7c578ff88258bf341b668df345e
Lucas <AgtLucas@users.noreply.github.com>
2016-01-17 09:30:40 -0200

    Update ex8.js

--- ex8.js
+++ ex8.js
@@ -1,7 +1,7 @@
 function duckCount() {
   return Array.prototype.slice.call(arguments).filter(function(obj) {
-    return Object.prototype.hasOwnProperty.call(obj, 'quack');
-  }).length;
+    return Object.prototype.hasOwnProperty.call(obj, 'quack')
+  }).length
 }
 
-module.exports = duckCount;
+module.exports = duckCount
